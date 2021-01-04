ca1fbba41fa7cdbee8dfa9bb1f583f44dd96d21f
Rafael Pérez <rapzadev@gmail.com>
2021-01-04 16:53:13 -0300

    Database postgres migrations created

---
+++ src/shared/infra/typeorm/migrations/1609732502409-AddOrderIdToOrdersProducts.ts
@@ -0,0 +1,41 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddOrderIdToOrdersProducts1609732502409
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'order_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'orders_products_Order',
+        columnNames: ['order_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'orders',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'orders_products',
+      'orders_products_Order',
+    );
+
+    await queryRunner.dropColumn('orders_products', 'order_id');
+  }
+}
